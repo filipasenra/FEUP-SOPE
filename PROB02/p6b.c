564ee417ef6345192bdccad3812b4a09faa6a56c
filipasenra <filipasenra4@gmail.com>
2019-03-06 15:56:30 +0000

    Finish 6

---
+++ PROB02/p6b.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+    DIR *dirp;
+    struct dirent *direntp;
+    struct stat stat_buf;
+    char *str;
+
+    //Is the number of argumnts correct?
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s dir_name\n", argv[0]);
+        exit(1);
+    }
+
+    //Tries to open the directory passed as argument
+    //If it fails, displays the error
+    if ((dirp = opendir(argv[1])) == NULL)
+    {
+        perror(argv[1]);
+        exit(2);
+    }
+
+    //Until it has not reached the end of the directory (more files to display)
+    while ((direntp = readdir(dirp)) != NULL)
+    {
+        //lstat returns information about the file in the stat_buf
+        if (lstat(direntp->d_name, &stat_buf) == -1) // testar com stat()
+        //if (stat(direntp->d_name, &stat_buf) == -1)
+        {
+            perror("lstat ERROR");
+            exit(3);
+        }
+
+        //Analizes type and mode of file
+        if (S_ISREG(stat_buf.st_mode))
+            str = "regular";
+        else if (S_ISDIR(stat_buf.st_mode))
+            str = "directory";
+        else
+            str = "other";
+        
+        //name of the file, file type
+        printf("%-25s - %-9s - i-none: %ld", direntp->d_name, str, stat_buf.st_ino);
+        if (str == "regular")
+        printf("  -size: %ld", stat_buf.st_size);
+
+        printf("\n");
+    }
+
+    closedir(dirp);
+    exit(0);
+}
